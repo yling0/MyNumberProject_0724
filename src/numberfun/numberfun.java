db05502922a04d4f91ed0653eaaa6959eca0d66e
yling0 <xyun2016@gmail.com>
2017-07-24 17:35:09 -0400

    commit of numberproject

---
+++ src/numberfun/numberfun.java
@@ -0,0 +1,13 @@
+package numberfun;
+
+public class numberfun {
+    public static void main(String[] args) {
+        for (int i=1; i<=100; i++) {
+        if (i%2==0) {
+            System.out.println(i+" is an even number");
+        } else {
+            System.out.println(i);
+        }
+        }
+    }
+}
